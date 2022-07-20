8020f6e7e5cb750019d5fa7510b27e470cabd15a
GLADYS <mungaiglado106@gmail.com>
2022-07-20 09:38:54 +0300

    intal commit

---
+++ src/components/MoviesShow.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import { useParams } from "react-router-dom";
+
+function MovieShow({ movies }) {
+    // call useParams to access thr params from the url
+
+    const params = useParams();
+    console.log(params);
+
+
+    return (
+        <div>
+            <h3>{movies[params.movieId].title}</h3>
+
+        </div>
+    );
+}
+
+export default MovieShow;
